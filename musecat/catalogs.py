cd02ff18e3aa222076d506c13b18ce21168a1b9d
Nicolas Tejos <ntejos@gmail.com>
2020-07-28 23:22:02 -0400

    initial structure of typical classes

---
+++ musecat/catalogs.py
@@ -0,0 +1,45 @@
+from astropy.table import Table
+"""Module for Catalog classes"""
+
+class Cat(object):
+    """Class to handle a generic MUSE catalog"""
+    def __init__(self, tab):
+        self.tab = tab  # astropy table object
+
+        #check if it has the minimum column names
+        self._check_minimum_req(self.tab)
+
+    def _check_minimum_req(self, tab):
+        """Checks for minimum requirements before initializing"""
+        colnames = tab.colnames
+        min_req = ['RA_d', 'Dec_d', 'z']
+        for req in min_req:
+            if req not in colnames:
+                raise ValueError('`{}` columname must be in catalog!'.format(req))
+
+
+
+class CatLSDCat(Cat):
+    """Class to handle main outputs of LSDCat"""
+
+    def __init__(self, tab):
+        super(CatLSDCat, self).__init__(tab)
+
+    @classmethod
+    def from_file(cls, filename):
+        """Init from a filename"""
+        tab = Table.read(filename)
+        return CatLSDCat(tab)
+
+
+class CatPyMUSECat(Cat):
+    """Class to handle main outputs of XXX"""
+
+    def __init__(self, tab):
+        super().__init__(self, tab)
+
+    @classmethod
+    def from_file(cls, filename):
+        """Init from a filename"""
+        tab = Table.read(filename)
+        return CatPyMUSECat(tab)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup 
+#from Cython.Build import cythonize
+
+setup(
+    name="musecat",
+    version="0.1.0",
+    description="Musecat: consolidated MUSE catalog.",
+    author="N. Tejos, J. Hernandez, C. Moya, F. Corro, et al.",
+    license="None",
+    author_email="ntejos@gmail.com",
+    url="https://github.com/ntejos/musecat",
+    packages=['musecat'])
+
